e9a36626c224c224ff75d6aabeee352e9adbeaf0
Luqi Yan <ylq.7719@gmail.com>
2017-06-25 16:01:40 -0700

    week 2.2 lecture

--- lecture/Week 2/2/SLList.java
+++ lecture/Week 2/2/SLList.java
@@ -9,22 +9,44 @@ public class SLList {
 		}
 	}
 
-	private IntNode first;
+	// private IntNode first;
+	private IntNode sentinel;
+	private int size;
+
+	public SLList() {
+		// first = null;
+		sentinel = new IntNode(77, null);
+		size = 0;
+	}
 
 	public SLList(int x) {
-		first = new IntNode(x, null);
+		// first = new IntNode(x, null);
+		sentinel = new IntNode(77, null);
+		sentinel.next = new IntNode(x, null);
+		size = 1;
 	}
 
 	public void addFirst(int x) {
-		first = new IntNode(x, first);
+		// first = new IntNode(x, first); 
+		sentinel.next = new IntNode(x, sentinel.next);
+		size += 1;
 	}
 
 	public int getFirst() {
-		return first.item;
+		// return first.item;
+		return sentinel.next.item;
 	}
 
 	public void addLast(int x) {
-		IntNode p = first;
+		size += 1;
+
+		// if (first == null) {
+		// 	first = new IntNode(x, n);
+		// 	return;
+		// }
+
+		// IntNode p = first;
+		IntNode p = sentinel;
 
 		while (p.next != null) {
 			p = p.next;	
@@ -33,19 +55,24 @@ public class SLList {
 		p.next = new IntNode(x, null);
 	}
 
-	private static int size(IntNode p) {
-		if (p.next == null) {
-			return 1;
-		} else {
-			return 1 + size(p.next);
-		}
-	}
+	// private static int size(IntNode p) {
+	// 	if (p.next == null) {
+	// 		return 1;
+	// 	} else {
+	// 		return 1 + size(p.next);
+	// 	}
+	// }
 
 	public int size() {
-		return size(first);
+		// return size(first);
+		return size;
 	}
 
 	public static void main(String[] args) {
+		SLList M = new SLList();
+		M.addLast(2);
+		System.out.println(M.getFirst());
+
 		SLList L = new SLList(15);
 		L.addFirst(10);
 		L.addFirst(5);
