8d84353309f80506045cb5e9eb73a220b9a62f86
Luqi Yan <ylq.7719@gmail.com>
2017-06-25 10:51:05 -0700

    week 2.2 - 1

--- lecture/Week 2/2/SLList.java
+++ lecture/Week 2/2/SLList.java
@@ -1,5 +1,15 @@
 public class SLList {
-	public IntNode first;
+	private static class IntNode {
+		public int item;
+		public IntNode next;
+
+		public IntNode(int i, IntNode n) {
+			item = i;
+			next = n;
+		}
+	}
+
+	private IntNode first;
 
 	public SLList(int x) {
 		first = new IntNode(x, null);
@@ -13,10 +23,35 @@ public class SLList {
 		return first.item;
 	}
 
+	public void addLast(int x) {
+		IntNode p = first;
+
+		while (p.next != null) {
+			p = p.next;	
+		}
+
+		p.next = new IntNode(x, null);
+	}
+
+	private static int size(IntNode p) {
+		if (p.next == null) {
+			return 1;
+		} else {
+			return 1 + size(p.next);
+		}
+	}
+
+	public int size() {
+		return size(first);
+	}
+
 	public static void main(String[] args) {
 		SLList L = new SLList(15);
 		L.addFirst(10);
 		L.addFirst(5);
+		System.out.println(L.size());
 		System.out.println(L.getFirst());
+		L.addLast(20);
+		System.out.println(L.size());
 	}
 }
