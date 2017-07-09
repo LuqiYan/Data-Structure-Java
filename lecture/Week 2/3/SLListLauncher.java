ff1669eda119e584a80d6fb0fc07a1753102e099
Luqi Yan <ylq.7719@gmail.com>
2017-07-08 23:09:59 -0700

    week 2.3, 2.4

---
+++ lecture/Week 2/3/SLListLauncher.java
@@ -0,0 +1,9 @@
+public class SLListLauncher {
+	public static void main(String[] args) {
+		SLList<String> s1 = new SLList<>("bone");
+		s1.addFirst("thugs");
+
+		System.out.println(s1.size());
+		System.out.println(s1.getFirst());
+	}
+}
